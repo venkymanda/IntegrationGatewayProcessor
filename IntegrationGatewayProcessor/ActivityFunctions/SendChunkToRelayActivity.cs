35425e8967140f1b83a0305e7d9774249cd4d5de
venkymanda <venkat.18power@gmail.com>
2023-10-16 19:49:25 +0530

    Adding Codium AI Improvements

--- IntegrationGatewayProcessor/ActivityFunctions/SendChunkToRelayActivity.cs
+++ IntegrationGatewayProcessor/ActivityFunctions/SendChunkToRelayActivity.cs
@@ -14,29 +14,28 @@ namespace IntegrationGatewayProcessor.ActivityFunctions
     [DurableTask(nameof(SendChunkToRelayActivity))]
     public class SendChunkToRelayActivity : TaskActivity<string, bool>
     {
-        private readonly ILogger logger;
+        private readonly ILogger<SendChunkToRelayActivity> _logger;
         private readonly IAzureRelaySenderService _azureRelaySenderService;
         private readonly IAzureRelayServiceHelper _azureRelayServiceHelper;
+        private static SemaphoreSlim _semaphore = new SemaphoreSlim(10); // Adjust the limit as per your needs for Conttrolling Parallel execution Limit
 
-        private static SemaphoreSlim semaphore = new SemaphoreSlim(10); // Adjust the limit as per your needs for Conttrolling Parallel execution Limit
-
-
-        public SendChunkToRelayActivity(ILogger<SendChunkToRelayActivity> logger,IAzureRelayServiceHelper azureRelayServiceHelper,IAzureRelaySenderService azureRelaySenderService) // activites have access to DI.
+        public SendChunkToRelayActivity(
+            ILogger<SendChunkToRelayActivity> logger,
+            IAzureRelayServiceHelper azureRelayServiceHelper,
+            IAzureRelaySenderService azureRelaySenderService)
         {
-            this.logger = logger;
+            _logger = logger;
             _azureRelaySenderService = azureRelaySenderService;
             _azureRelayServiceHelper = azureRelayServiceHelper;
         }
 
-
-        public async override Task<bool> RunAsync(TaskActivityContext context, string input)
+        public override async Task<bool> RunAsync(TaskActivityContext context, string input)
         {
-            await semaphore.WaitAsync();
+            await _semaphore.WaitAsync();
 
             try
             {
-
-                logger.LogInformation("Saying hello to {name}.", input);
+                _logger.LogInformation("Saying hello to {name}.", input);
 
                 await _azureRelaySenderService.SendFileAsync(input);
                 return true;
@@ -47,10 +46,11 @@ namespace IntegrationGatewayProcessor.ActivityFunctions
             }
             finally
             {
-                semaphore.Release();
+                _semaphore.Release();
             }
         }
     }
+
 
 
 
