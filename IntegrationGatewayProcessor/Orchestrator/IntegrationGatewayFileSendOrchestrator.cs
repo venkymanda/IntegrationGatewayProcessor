7f44dfd991b651fb85547b08b088ee8252707688
venkymanda <venkat.18power@gmail.com>
2023-11-09 23:56:13 +0530

    FileSenderOrchestrator Returning Failed Chunk Information and Improving error handling

--- IntegrationGatewayProcessor/Orchestrator/IntegrationGatewayFileSendOrchestrator.cs
+++ IntegrationGatewayProcessor/Orchestrator/IntegrationGatewayFileSendOrchestrator.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using IntegrationGatewayProcessor.Models;
 using Newtonsoft.Json;
+using Polly;
 
 namespace IntegrationGatewayProcessor.Orchestrator
 {
@@ -52,35 +53,85 @@ namespace IntegrationGatewayProcessor.Orchestrator
                 // Wait for all tasks to complete before continuing
                 await Task.WhenAll(tasks);
 
+                // Check if any tasks failed
+                bool allTasksSucceeded = !tasks.Any(t => t.IsFaulted);
                 // If you need additional orchestration logic, add it here.
 
                 // After the main logic, add the callback URL and payload
+           
+                var allchunksdelivered=tasks.All(i=>i.Result.Item1);
+                List<int> failedchunks = new List<int>();
+                if (allchunksdelivered)
+                {
+                    // Iterate over the tasks list
+                    foreach (var task in tasks)
+                    {
+                        // Access the tuple values
+                        bool success = task.Result.Item1;
+                           
+                        if(!success)
+                        {
+                            //Add Sequnec That failed from task
+                            failedchunks.Add(task.Result.Item2);
+                        };
 
+                    }
+                        
+                }
+                int failedtasks=tasks.Where(i=>i.Result.Item1==false).Count();
                 var successPayload = new SuccessCallbackDTO
                 {
                     Status = "success",
                     Message = "File transfer completed successfully",
-                    ProcessedChunks = totalChunks,
-                    TotalBytesProcessed = totalChunks*chunkSize,
+                    ProcessedChunks = totalChunks-failedtasks,
+                    TotalBytesProcessed = totalChunks * chunkSize,
                     TransactionId = inputRequestDTO.TransactionId,
-                    DocumentId = $"{inputRequestDTO.BlobContainerName}\\{inputRequestDTO.BlobName}"
+                    DocumentId = $"{inputRequestDTO.BlobContainerName}\\{inputRequestDTO.BlobName}",
+                    FailedChunks=failedchunks,
                 };
 
                 string callbackPayload = JsonConvert.SerializeObject(successPayload); // You may need to use a JSON library, like Newtonsoft.Json
 
                 string callbackUrl = inputRequestDTO.CallbackURL;
-                
+
 
                 // Make the HTTP callback at the end of execution
-                var callbackstatus=(inputRequestDTO.DoCallBack)?await MakeHttpCallbackAsync(callbackUrl, callbackPayload):true;
+                var callbackstatus = (inputRequestDTO.DoCallBack) ? await MakeHttpCallbackAsync(callbackUrl, callbackPayload, logger) : true;
 
                 // If you reach this point, it means the execution was successful
                 logger.LogInformation("File transfer completed successfully.");
                 return callbackstatus;
+                
 
 
 
             }
+
+            catch (AggregateException ae)
+            {
+                foreach (var innerException in ae.InnerExceptions)
+                {
+                    // Handle each inner exception individually
+                    logger.LogError($"An error occurred: {innerException.Message}");
+                    
+                }
+                // Make the HTTP callback in case of error
+                var failurePayload = new FailureCallbackDTO
+                {
+                    Status = "failure",
+                    ErrorMessage = "An error occurred during file transfer",
+                    ErrorDetails = ae.ToString(),
+                    TransactionId = inputRequestDTO.TransactionId,
+                    DocumentId = $"{inputRequestDTO.BlobContainerName}\\{inputRequestDTO.BlobName}"
+                };
+
+                string payloadJson = JsonConvert.SerializeObject(failurePayload); // You may need to use a JSON library, like Newtonsoft.Json
+
+                if (inputRequestDTO.DoCallBack)
+                    await MakeHttpCallbackAsync(inputRequestDTO.CallbackURL, payloadJson, logger);
+                return false; // Indicate that this task failed.
+            }
+
             catch (Exception ex)
             {
                 logger.LogError($"An error occurred: {ex.Message}");
@@ -97,7 +148,7 @@ namespace IntegrationGatewayProcessor.Orchestrator
                 string payloadJson = JsonConvert.SerializeObject(failurePayload); // You may need to use a JSON library, like Newtonsoft.Json
 
                 if (inputRequestDTO.DoCallBack) 
-                    await MakeHttpCallbackAsync(inputRequestDTO.CallbackURL, payloadJson);
+                    await MakeHttpCallbackAsync(inputRequestDTO.CallbackURL, payloadJson,logger);
 
                 return false; // Indicate that this task failed.
 
@@ -106,7 +157,7 @@ namespace IntegrationGatewayProcessor.Orchestrator
 
         // Implement other functions and interfaces as needed
 
-        private static async Task<bool> ProcessAndSendChunkAsync(
+        private static async Task<(bool,int)> ProcessAndSendChunkAsync(
                                   TaskOrchestrationContext context,
                                   string blobContainerName,
                                   string blobName,
@@ -128,7 +179,7 @@ namespace IntegrationGatewayProcessor.Orchestrator
                 };
                 var outputDTO                 = await context.CallActivityAsync<BlobDTO>("GetChunkDataActivity", blobDTO);
                 bool sentSuccessfully         = await context.CallActivityAsync<bool>("SendChunkToRelayActivity", outputDTO);
-                return sentSuccessfully;
+                return (sentSuccessfully,currentChunkSequence);
             }
             catch (Exception ex)
             {
@@ -138,21 +189,34 @@ namespace IntegrationGatewayProcessor.Orchestrator
                 // Log the error
                 context.SetCustomStatus($"Error processing chunk {currentChunkSequence}: {ex.Message}");
 
-                return false; // Indicate that this task failed.
+                return (false,currentChunkSequence); // Indicate that this task failed.
             }
         }
 
        
        
 
-    public static async Task<bool> MakeHttpCallbackAsync(string callbackUrl, string payload)
+    public static async Task<bool> MakeHttpCallbackAsync(string callbackUrl, string payload,ILogger logger)
     {
         using (HttpClient httpClient = new HttpClient())
         {
-            var content = new StringContent(payload, Encoding.UTF8, "application/json");
-            var response = await httpClient.PostAsync(callbackUrl, content);
-            return response.IsSuccessStatusCode;
-        }
+
+                var content = new StringContent(payload, Encoding.UTF8, "application/json");
+                var response = await httpClient.PostAsync(callbackUrl, content);
+
+                // Log response status
+                if (response.IsSuccessStatusCode)
+                {
+                    logger.LogInformation($"HTTP callback successful: {response.StatusCode}");
+                }
+                else
+                {
+                    logger.LogError($"HTTP callback failed: {response.StatusCode}");
+                }
+
+                return response.IsSuccessStatusCode;
+
+            }
     }
 
 
